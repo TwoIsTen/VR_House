a98cc4af2f958c95730bc508b5c07bc06ab074c4
TwoIsTen <gtx2080@gmail.com>
2017-03-18 11:51:24 +0800

    create project
    create project on ue4.13

---
+++ Source/VR_House/VR_HouseGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_House.h"
+#include "VR_HouseGameMode.h"
+
+
+
+
---
+++ Source/VR_House/VR_HouseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VR_HouseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_HOUSE_API AVR_HouseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
